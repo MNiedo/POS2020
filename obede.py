da4ba1ed296d02871d6c43664d3b860846907222
MNiedo <mmniedostatkiewicz+github@gmail.com>
2020-06-12 17:25:00 +0200

    Dodanie większej ilości wyświetlanych zmiennych
    Prawdopodobnie finalny commit

--- obede.py
+++ obede.py
@@ -10,8 +10,6 @@ SERIAL_PORT_TIME_OUT = 60 # Czas, po którym nastąpi poddanie się
 ELM_CONNECT_SETTLE_PERIOD = 5 # Czasy do ponownego połączenia
 ELM_CONNECT_TRY_COUNT = 5 # Ilość prób do połączenia, póki nie będzie katastrofalnego błędu i nie spali się wszystko
 
-
-
 connection = obd.OBD() # auto connect
 
 ports = obd.scan_serial() # Próba automatycznego skanowania portu
@@ -50,13 +48,45 @@ else:
 
 # Czas na wyświetlanie danych
 
-obroty = connection.query(obd.commands.RPM) # Zwraca obroty z obd
+obroty = connection.query(obd.commands.RPM) # Zwraca wartość obrotów z obd (x1)
+predkosc = connection.query(obd.commands.SPEED) # Zwraca wartość prędkości z obd (km/h)
+stanpaliwa = connection.query(obd.commands.FUEL_STATUS) # Zwraca wartość stanu paliwa (string)
+statusczek = connection.query(obd.commands.STATUS) # Zwraca czy były czekendżiny (string)
+tempborygo = connection.query(obd.commands.COOLANT_TEMP) # Zwraca wartość temperatury płynu chłodniczego (stopnie celsjusza)
+kolektorcis = connection.query(obd.commands.INTAKE_PRESSURE) # Zwraca wartość ciśnienia w kolektorze dolotowym (kPa)
+paliwocis = connection.query(obd.commands.FUEL_PRESSURE) # Zwraca wartość ciśnienia paliwa (kPa)
+wlottemp = connection.query(obd.commands.INTAKE_TEMP) # Zwraca wartość temperatury na wlocie powietrza (kPa)
+maswlotpow = connection.query(obd.commands.MAF)# Zwraca wartość strumienia masy wlotu powietrza (g/s)
+czaspracy = connection.query(obd.commands.RUN_TIME) # Zwraca wartość czasu pracy silnika (s)
+egrwartosc = connection.query(obd.commands.COMMANDED_EGR) # Zwraca wartość narzuconej EGRowi (%)
+egrbledy = connection.query(obd.commands.EGR_ERROR) # Zwraca wartość błędów EGRa (%)
+ilepaliwa = connection.query(obd.commands.FUEL_LEVEL) # Zwraca wartość ilości paliwa (UWAGA: %)
+ciszew = connection.query(obd.commands.BAROMETRIC_PRESSURE) # Zwraca wartość ciśnienia otoczenia z czujnika barometrycznego (kPa)
+napmodkontr = connection.query(obd.commands.CONTROL_MODULE_VOLTAGE) # Zwraca wartość napięcia na module kontrolnym (V)
+calobcsil = connection.query(obd.commands.ABSOLUTE_LOAD) # Zwraca wartość całkowitego obciążenia silnika (%)
+pedalgazu = connection.query(obd.commands.RELATIVE_ACCEL_POS) # Zwraca wartość relatywnej pozycji pedału gazu (%)
+tempoleju = connection.query(obd.commands.OIL_TEMP) # Zwraca wartość temperatury oleju silnikowego (stopnie celsjusza)
+czaswtryskpal = connection.query(obd.commands.FUEL_INJECT_TIMING) # Zwraca wartość czasu wtrysku paliwa (stopnie)
+spalanie = connection.query(obd.commands.FUEL_RATE) # Zwraca wartość spalania paliwa (UWAGA: l/h)
 
-
-
-
-
-print ("Obroty: ",obroty, " RPM\n\n")
-
-
-
+# Wyświetlanie wartości
+print ("Obroty: ",obroty," RPM\n\n")
+print ("Prędkosc: ",predkosc," RPM\n\n")
+print ("Wartosc stanu paliwa: ",stanpaliwa,"\n\n")
+print ("Czy byly czekendziny: ",statusczek,"\n\n")
+print ("Temp plynu chlodniczego: ",tempborygo,"°C\n\n")
+print ("Cisnienie w kolektorze dolotowym: ",kolektorcis,"kPa\n\n")
+print ("Cisnienie paliwa: ",paliwocis,"kPa\n\n")
+print ("Temp na wlocie powietrza: ",wlottemp,"°C\n\n")
+print ("Masa wlotu powietrza: ",maswlotpow,"g/s\n\n")
+print ("Czas pracy silnika :",czaspracy,"s\n\n")
+print ("Wartosc EGR: ",egrwartosc,"%\n\n")
+print ("Bledy EGR: ",egrbledy,"%\n\n")
+print ("Ilosc paliwa: ",ilepaliwa,"%\n\n")
+print ("Cisnienie zewnetrzne: ",ciszew,"kPa\n\n")
+print ("Napiecie na module kontrolnym: ",napmodkontr,"V\n\n")
+print ("Obciazenie silnika: ",calobcsil,"%\n\n")
+print ("Pozycja pedalu gazu: ",pedalgazu,"%\n\n")
+print ("Temp oleju: ",tempoleju,"°C\n\n")
+print ("Wtrysk paliwa: ",czaswtryskpal,"°\n\n")
+print ("Spalanie paliwa: ",spalanie,"l/h\n\n")
